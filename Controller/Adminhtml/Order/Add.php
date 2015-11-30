0993507c7b841403e657965553e90c769654f324
mengento <mallwell@qq.com>
2015-11-30 16:05:37 +0800

    Create Add.php

---
+++ Controller/Adminhtml/Order/Add.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Mengento\OrderRemark\Controller\Adminhtml\Order;
+
+class Add extends \Mengento\OrderRemark\Controller\Adminhtml\Order
+{
+
+    public function execute()
+    {
+	$order = $this->_initOrder();
+      if ($order) {
+        try {
+
+        $data = $this->getRequest()->getPost('remark');
+        $comment = array(
+                'increment_id'=>$order->getIncrementId(),
+                'entity_id'=>$order->getId(),
+                'created_at'=> date('Y-m-d H:i:s'),
+                'user'=>$this->getUser()->getFirstname(),
+                'remark'=> $data['comment'],
+            );
+	$remark = $this->_initRemark();
+	$remark->setData($comment);
+	$remark->save();
+	return $this->resultPageFactory->create();
+        } catch (\Magento\Framework\Exception\LocalizedException $e) {
+                $response = ['error' => true, 'message' => $e->getMessage()];
+        } catch (\Exception $e) {
+                $response = ['error' => true, 'message' => __('We cannot add order history.')];
+        }
+            if (is_array($response)) {
+                $resultJson = $this->resultJsonFactory->create();
+                $resultJson->setData($response);
+                return $resultJson;
+            }
+        }
+        return $this->resultRedirectFactory->create()->setPath('sales/*/');
+    }
+}
